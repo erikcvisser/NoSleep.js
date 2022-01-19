3e19341bfbfe178af870e412a440f888399d0a5b
Erik Visser <erikcvisser@gmail.com>
2022-01-19 13:57:33 +0100

    rename nosleep title to ergo mate

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ class NoSleep {
       // Set up no sleep video element
       this.noSleepVideo = document.createElement("video");
 
-      this.noSleepVideo.setAttribute("title", "No Sleep");
+      this.noSleepVideo.setAttribute("title", "Ergo Mate");
       this.noSleepVideo.setAttribute("playsinline", "");
 
       this._addSourceToVideo(this.noSleepVideo, "webm", webm);
